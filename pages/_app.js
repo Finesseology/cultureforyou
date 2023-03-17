69c7f1f6512d5efb6610f8a93506d200ff7d72f6
Jui-Ching Chen <92921416+jujuchen5@users.noreply.github.com>
2023-03-17 16:01:34 -0700

    D8-18
    Layout.js is imported into App in order for the Header.js, NavBar.js, and Footer.js to show globally without coding on every page individually.
    
    Wrapping everything in Layout and importing it here keeps _app.js clean.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+/**Wrapping the components {..pageProps}
+ * in Layout from Layout.js in components
+ * This way we can reuse the Header and NavBar on every page without 
+ * coding one for every single page.
+ */
+
+import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <>
+    <Layout>
+    <Component {...pageProps}/>
+    </Layout>
+  </>
 }
