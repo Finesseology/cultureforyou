687a53366aefcac48700cf58d64c5c519f7e54d3
Chris <cwlemke@live.com>
2023-03-17 17:48:53 -0700

    # D8-31 Removed unused image import files.

--- pages/shopLayout.js
+++ pages/shopLayout.js
@@ -1,24 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/ShopPageStyles.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/ShopPageStyles.module.css";
 
-
-import HennaPic from 'pages/images/hennaTest.png'
-import Engraving from 'pages/images/engraving.png'
-import WeddingSign from 'pages/images/weddingSign.png'
-import Topper from 'pages/images/toppers.png'
-import Makeup from 'pages/images/makeup.png'
-import Logo from 'pages/images/cultureforyoulogo.png'
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function shopLayout() {
-  return (
-   
-      <main className ={styles.main}>
-        <div>This is shop page</div>
-      </main>
-
-  )
+	return (
+		<main className={styles.main}>
+			<div>This is shop page</div>
+		</main>
+	);
 }
