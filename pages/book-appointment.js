cb4d306dc04cfd19bd56a6e217c88c8ca86073e9
aliag123 <arifghuncha@gmail.com>
2023-04-14 18:32:18 -0700

    #D8-40
    book and confirm appointment

--- pages/book-appointment.js
+++ pages/book-appointment.js
@@ -1,91 +1,98 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import styles from "@/styles/AppointmentBooking.module.css";
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useState } from "react";
+import { useEffect } from 'react';
 
-function BookingForm() { 
-  const [selectedDate, setSelectedDate] = React.useState(null);
+
+const  BookingForm = ()  =>{
   const router = useRouter();
-  const [email, setEmail] = useState(); 
-    const { status } = useSession({
-      required: true,
-      onUnauthenticated() {
-        router.push('/signInPage'); 
+  const [email, setEmail] = useState();
+  const [option, setType] = useState();
+  const [date, setDate] = useState();
+  const [time, setTime] = useState('');
+
+  useEffect(() => { //retrieve date and time from api
+  const {date, time} = router.query;
+  setDate(date);
+  setTime(time);
+  }, [router.query]);
+
+
+  //PROTECTED PAGE
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+        router.push('/signInPage');
         return <p>Redirecting you to the login page...</p>
-      },
-    })
+    },
+  })
 
-    if (status === "loading") {
-        return <p>Loading...</p>
-    }
+  if (status === "loading") {
+       return <p>Loading...</p>
+  }
 
-    function handleEmailChange(event) {
-      setEmail(event.target.value);
-    }
+   function handleEmailChange(event) {
+     setEmail(event.target.value);
+   }
 
-   const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-          const response = await fetch("/api/email", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ to: email, subject: "Appointment confirmed for " + date, text: '<p>Your appointment has been confirmed.</p>' }),
-          });
-          if (response.ok) {
-              setMessage("Email sent successfully");
-          } else {
-              setMessage("Failed to send email");
-          }
-      } catch (error) {
-          console.error(error);
-          setMessage("Failed to send email");
-      }
-  };
+   function handleAppointmentType(event) {
+    setType(event.target.value);
+  }
 
- return ( 
-   <div className={styles.bookingForm}>
-       <center>
-     <h2>Book an Appointment</h2>
-     <form>
-       <div className={styles.formGroup}>
-         <DatePicker
-           id="date"
-           selected={selectedDate}
-           onChange={date => setSelectedDate(date)}
-           timeIntervals={60}
-           minDate={new Date()}
-           minTime={new Date().setHours(9, 0)}
-           maxTime={new Date().setHours(16, 0)}
-           dateFormat="MMMM d, yyyy h:mm aa"
-           showTimeSelect
-           timeFormat="HH:mm"
-           timeCaption="Time"
-           filterDate={(date) => date.getDay() !== 0 && date.getDay() !== 6}
-           placeholderText="Select a date and time"
-           shouldCloseOnSelect
-           className={styles.datepicker}
-           />
-       </div>
-       <div className={styles.formGroup}>
-       <form onSubmit={handleSubmit}>
-      <label> 
-        Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </label><div>
-            <button type="submit" className={styles.submitButton}>Submit</button>
-            </div>
-            </form>
-       </div>
-     </form>
-     </center>
-   </div>
- );
+
+  //EMAIL CONFIRMATION TO USER
+  const handleSubmit = async (e) => {
+     e.preventDefault();
+     try {
+         const response = await fetch("/api/email", {
+             method: "POST",
+             headers: {
+                 "Content-Type": "application/json",
+             },
+             body: JSON.stringify({ to: email, subject: '<p>Your appointment has been confirmed.</p>', text: option + " appointment confirmed for " + thiss + " at " + time }),
+         });
+         if (response.ok) {
+          console.log("Email sent");
+         } else {
+             console.log("email not snet");
+         }
+     } catch (error) {
+         console.error(error);
+     }
+ };
+
+return (
+  <div className={styles.bookingForm}>
+  <center>
+    <h2>APPOINTMENT CONFIRMATION</h2>
+    <form>
+      <label htmlFor="options"> Choose appointment type: </label> 
+      <select id="options" name="options" value={option} onChange={handleAppointmentType}> 
+        <option value="Henna">Henna</option>
+        <option value="Makeup">Makeup</option>
+        <option value="Makeup & Henna">Both</option>
+      </select>
+      <br />
+      <div>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange}/>
+      </label>
+     </div>
+     <div>
+      <p>Your Appointment Details: {option} appointment on {date} at {time}</p>
+      <p>If no appointments visible please go back and select a date and time</p>
+     </div>
+     <div>
+        <button type="submit" onSubmit={handleSubmit} className={styles.submitButton}>Submit</button>
+     </div>
+    </form>
+  </center>
+  </div>
+);
 }
-
 
 export default BookingForm;
