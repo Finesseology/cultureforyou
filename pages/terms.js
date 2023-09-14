6e5a443c052a72f8ac6d6ed31d7b51fd3bf67662
Chris <cwlemke@live.com>
2023-09-14 11:25:49 -0700

    D8-197 Removed old unused files from project

--- pages/terms.js
+++ pages/terms.js
@@ -1,72 +1,96 @@
-
-import styles from '@/styles/Terms.module.css'
+import styles from "@/styles/terms.module.css";
 
 import Head from "next/head";
 export default function Terms() {
-return (
+	return (
+		<>
+			<br></br>
+			<br></br>
+			<h1 className={styles.h1}>
+				<center>Terms of Service</center>
+			</h1>
 
-<>  
-                <br></br>
-                <br></br>
-                <h1 className={styles.h1}><center>Terms of Service</center></h1>
-  
-                <center>
-                    <div className={styles.myDiv}>
-                        <h2 className={styles.h2}>General Terms</h2>
-                        <p className={styles.p}>By accessing this web site, you are agreeing to be bound by this Agreement.
-                            Culture For You reserves the right to update or modify this Agreement at any time without notice. 
-                            If you do not agree to these terms, you are prohibited from accessing or using this site.
-                            Last updated: March 11, 2023      
-                            Please read these Terms of Use (“Terms”, “Terms of Use”) carefully before using t
-                            he https://cultureforu.com website (the “Service”) operated by Culture For You (“us”, “we”, or “our”).
-                            Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. 
-                            These Terms apply to all visitors, users and others who access or use the Service.
-                            By accessing or using the Service you agree to be bound by these Terms. 
-                            If you disagree with any part of the terms then you may not access the Service.<br></br></p>
+			<center>
+				<div className={styles.myDiv}>
+					<h2 className={styles.h2}>General Terms</h2>
+					<p className={styles.p}>
+						By accessing this web site, you are agreeing to be bound by this Agreement. Culture For You
+						reserves the right to update or modify this Agreement at any time without notice. If you do not
+						agree to these terms, you are prohibited from accessing or using this site. Last updated: March
+						11, 2023 Please read these Terms of Use (“Terms”, “Terms of Use”) carefully before using t he
+						https://cultureforu.com website (the “Service”) operated by Culture For You (“us”, “we”, or
+						“our”). Your access to and use of the Service is conditioned on your acceptance of and
+						compliance with these Terms. These Terms apply to all visitors, users and others who access or
+						use the Service. By accessing or using the Service you agree to be bound by these Terms. If you
+						disagree with any part of the terms then you may not access the Service.<br></br>
+					</p>
 
-                        <h2 className={styles.h2}>Accounts</h2>
-                        <p className={styles.p}>When you create an account with us, you must provide us information that is accurate, complete, 
-                            and current at all times. Failure to do so constitutes a breach of the Terms, which may result in 
-                            immediate termination of your account on our Service. You are responsible for safeguarding the password that you
-                            use to access the Service and for any activities or actions under your password, whether your password is with our
-                            Service or a third-party service. You agree not to disclose your password to any third party. 
-                            You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account. </p>
+					<h2 className={styles.h2}>Accounts</h2>
+					<p className={styles.p}>
+						When you create an account with us, you must provide us information that is accurate, complete,
+						and current at all times. Failure to do so constitutes a breach of the Terms, which may result
+						in immediate termination of your account on our Service. You are responsible for safeguarding
+						the password that you use to access the Service and for any activities or actions under your
+						password, whether your password is with our Service or a third-party service. You agree not to
+						disclose your password to any third party. You must notify us immediately upon becoming aware of
+						any breach of security or unauthorized use of your account.{" "}
+					</p>
 
+					<h2 className={styles.h2}>Links To Other Web Sites</h2>
+					<p className={styles.p}>
+						Our Service may contain links to third-party web sites or services that are not owned or
+						controlled by Culture For You. Culture For You has no control over, and assumes no
+						responsibility for, the content, privacy policies, or practices of any third party web sites or
+						services. You further acknowledge and agree that Culture For You shall not be responsible or
+						liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in
+						connection with use of or reliance on any such content, goods or services available on or
+						through any such web sites or services. We strongly advise you to read the terms and conditions
+						and privacy policies of any third-party web sites or services that you visit.{" "}
+					</p>
 
-                        <h2 className={styles.h2}>Links To Other Web Sites</h2>
-                        <p className={styles.p}>Our Service may contain links to third-party web sites or services that are not owned or controlled by Culture For You.
-                            Culture For You has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Culture For You shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.
-                            We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit. </p>
-                        
-                        <h2 className={styles.h2}>Termination</h2>
-                        <p className={styles.p}>We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.
-                            Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account, you may simply discontinue using the Service.
-                            All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability. </p>
-                        
-                        <h2 className={styles.h2}>Disclaimer</h2>
-                        <p className={styles.p}>Your use of the Service is at your sole risk. The Service is provided on an “AS IS” and “AS AVAILABLE” basis. The Service is provided without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement or course of performance. </p>
+					<h2 className={styles.h2}>Termination</h2>
+					<p className={styles.p}>
+						We may terminate or suspend your account immediately, without prior notice or liability, for any
+						reason whatsoever, including without limitation if you breach the Terms. Upon termination, your
+						right to use the Service will immediately cease. If you wish to terminate your account, you may
+						simply discontinue using the Service. All provisions of the Terms which by their nature should
+						survive termination shall survive termination, including, without limitation, ownership
+						provisions, warranty disclaimers, indemnity and limitations of liability.{" "}
+					</p>
 
-                        <h2 className={styles.h2}>Governing Law</h2>
-                        <p className={styles.p}>These Terms shall be governed and construed in accordance with the laws of United States without regard to its conflict of law provisions.
-                            Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service. </p>
+					<h2 className={styles.h2}>Disclaimer</h2>
+					<p className={styles.p}>
+						Your use of the Service is at your sole risk. The Service is provided on an “AS IS” and “AS
+						AVAILABLE” basis. The Service is provided without warranties of any kind, whether express or
+						implied, including, but not limited to, implied warranties of merchantability, fitness for a
+						particular purpose, non-infringement or course of performance.{" "}
+					</p>
 
-                        <h2 className={styles.h2}>Changes</h2>
-                        <p className={styles.p}>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. 
-                            If a revision is material we will try to provide at least 15 days notice prior to any new terms taking effect. 
-                            What constitutes a material change will be determined at our sole discretion.
-                            By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. 
-                            If you do not agree to the new terms, please stop using the Service. </p>
-                        
-                        <h2 className={styles.h2}>Contact Us</h2>
-                        <p className={styles.p}>If you have any questions about these Terms, please contact us.</p>
-                        <p className={styles.p}>Phone Number: (209) 810 - 5779</p>
-                        <p className={styles.p}>Email: cultureforu1@gmail.com</p>
-                    </div>
-                </center>
+					<h2 className={styles.h2}>Governing Law</h2>
+					<p className={styles.p}>
+						These Terms shall be governed and construed in accordance with the laws of United States without
+						regard to its conflict of law provisions. Our failure to enforce any right or provision of these
+						Terms will not be considered a waiver of those rights. If any provision of these Terms is held
+						to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain
+						in effect. These Terms constitute the entire agreement between us regarding our Service, and
+						supersede and replace any prior agreements we might have between us regarding the Service.{" "}
+					</p>
 
+					<h2 className={styles.h2}>Changes</h2>
+					<p className={styles.p}>
+						We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a
+						revision is material we will try to provide at least 15 days notice prior to any new terms
+						taking effect. What constitutes a material change will be determined at our sole discretion. By
+						continuing to access or use our Service after those revisions become effective, you agree to be
+						bound by the revised terms. If you do not agree to the new terms, please stop using the Service.{" "}
+					</p>
 
-</> 
-
-)
+					<h2 className={styles.h2}>Contact Us</h2>
+					<p className={styles.p}>If you have any questions about these Terms, please contact us.</p>
+					<p className={styles.p}>Phone Number: (209) 810 - 5779</p>
+					<p className={styles.p}>Email: cultureforu1@gmail.com</p>
+				</div>
+			</center>
+		</>
+	);
 }
-
