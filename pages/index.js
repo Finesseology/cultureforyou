624c782cbf473c2e63a672847d0cec4a6d112c8a
Ally <amahbobi@csus.edu>
2023-09-15 13:39:51 -0700

    D8-166 Redesigned homepage

--- pages/index.js
+++ pages/index.js
@@ -7,131 +7,167 @@ import styles from "@/styles/homepage.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+	
 	return (
+	
 		<main className={styles.mainContainer}>
-			<div className={styles.introContainer}>
-				<div className={styles.introTitle}>Welcome to Culture For You!</div>
-				<div className={styles.introDesc}>
-					Check out our one of a kind designs! Here we provide Henna services, Engraving, Toppers, and Wedding
-					Signs.
-				</div>
-			</div>
+			 
+				<div className={styles.introContainer}>
+					<div className={styles.introTitle}></div>
+					<div className={styles.centerImage}>
+								<Image
+									className={styles.centeredImage}
+									src={"/homepagePics/transparent.png"}
+									alt="Toppers"
+									width={350}
+									height={400}
+									priority />
+</div>
 
-			<div className={styles.center}>
-				<div className={styles.container}>
-					<div className={styles.row}>
-						<div className={styles.column66}>
-							<h1 className={styles.sectionTitle}>Henna Service</h1>
-							<p>
-								We offer henna services by appointment! We do henna for all occasions, including special
-								occasions or just for fun! Our henna services are of the highest quality and we use only
-								the best henna products. Check out our henna designs and shop for henna products!
-							</p>
+					<div className={styles.introDesc}>
 
-							<a href="./henna-showcase" className={styles.button}>
-								Shop
-							</a>
+					
+
+					</div>
+					<div className={styles.servicesIntro}>
+						
+					<div className={styles.shopNavBarContainer}>
+							
+							<a href='./shop-layout' className={styles.active}>All</a>
+							<a  className={styles.shopNavBarLine}>|</a>
+							<a href='./henna-showcase'  className={styles.shopNavBar}>Henna</a>
+							<a className={styles.shopNavBarLine}>|</a>
+							<a href='./topper' className={styles.shopNavBar}>Topper</a>
+							<a className={styles.shopNavBarLine}>|</a>
+							<a href='./engraving-showcase' className={styles.shopNavBar}>Engraving</a>
+							<a  className={styles.shopNavBarLine}>|</a>
+							<a href='./wedding-sign' className={styles.shopNavBar}>Wedding Sign</a>
 						</div>
-						<div className={styles.column33}>
-							<Image
-								className={styles.imgStyle}
-								src={"/hennaPics/henna5.png"}
-								alt="Toppers"
-								width={250}
-								height={300}
-								priority
-							/>
+								
+							</div>
+
+					
+
+				</div>
+
+
+
+				
+
+
+				<div className={styles.centerHome}>
+					<div className={styles.container}>
+						<div className={styles.row}>
+							<div className={styles.column66}>
+								<h1 className={styles.sectionTitle}>Henna Service</h1>
+								<p>
+									We offer henna services by appointment. We offer personalized henna designs for all occasions.
+									Our henna services are of the highest quality and we only use 100% organic henna.
+									Click below to browse some of our designs.
+								</p>
+
+								<a href="./henna-showcase" className={styles.button}>
+									Henna Showcase
+								</a>
+							</div>
+							<div className={styles.colum33}>
+								<Image
+									className={styles.imgStyle}
+									src={"/hennaPics/henna8.png"}
+									alt="Toppers"
+									width={250}
+									height={300}
+									priority />
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className={styles.center}>
-				<div className={styles.container}>
-					<div className={styles.row}>
-						<div className={styles.column33}>
-							<Image
-								className={styles.imgStyle}
-								src={"/index/toppers.png"}
-								alt="Toppers"
-								width={250}
-								height={300}
-								priority
-							/>
-						</div>
-						<div className={styles.column66}>
-							<h1 className={styles.sectionTitle}>Topper</h1>
-							<p>
-								We do Topper for all sorts of special occasions. Our wedding toppers are sure to make
-								any celebration extra special. offer a wide range of toppers, including wedding toppers,
-								birthday toppers, and more! Check out our topper designs and shop for toppers!
-							</p>
-							<a href="./topper" className={styles.button}>
-								Shop
-							</a>
+				<div className={styles.centerHome}>
+					<div className={styles.container}>
+						<div className={styles.row}>
+							<div className={styles.column33}>
+								<Image
+									className={styles.imgStyle}
+									src={"/topperPics/topper2.png"}
+									alt="Toppers"
+									width={250}
+									height={300}
+									priority />
+							</div>
+							<div className={styles.column66}>
+								<h1 className={styles.sectionTitle}>Toppers</h1>
+								<p>
+									We do toppers for all sorts of special occasions. Our wedding toppers are sure to make
+									any celebration extra special. We offer a wide range of toppers, including wedding toppers,
+									birthday toppers, and more! Check out our topper designs and shop for toppers!
+								</p>
+								<a href="./topper" className={styles.button}>
+									Toppers Showcase
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className={styles.center}>
-				<div className={styles.container}>
-					<div className={styles.row}>
-						<div className={styles.column66}>
-							<h1 className={styles.sectionTitle}>Engraving</h1>
-							<p>
-								We do engraving for all types of products! Get your lovers name engraved on a starbucks
-								cup, or get your name engraved on a your wine glass! We offer a wide range of engraving
-								services, including sports, glass, floral, and more! Check out our engraving designs and
-								shop for engraving products!
-							</p>
+				<div className={styles.centerHome}>
+					<div className={styles.container}>
+						<div className={styles.row}>
+							<div className={styles.column66}>
+								<h1 className={styles.sectionTitle}>Engraving</h1>
+								<p>
+									We do engraving for all types of products! Get your lovers name engraved on a starbucks
+									cup, or get your name engraved on a your wine glass! We offer a wide range of engraving
+									services, including sports, glass, floral, and more! Check out our engraving designs and
+									shop for engraving products!
+								</p>
 
-							<a href="./engraving-showcase" className={styles.button}>
-								Shop
-							</a>
-						</div>
-						<div className={styles.column33}>
-							<Image
-								className={styles.imgStyle}
-								src={"/index/engraving.png"}
-								alt="Engraving"
-								width={250}
-								height={300}
-								priority
-							/>
+								<a href="./engraving-showcase" className={styles.button}>
+									Engraving Showcase
+								</a>
+							</div>
+							<div className={styles.column33}>
+								<Image
+									className={styles.imgStyle}
+									src={"/index/YellowFloralWreath.png"}
+									alt="Engraving"
+									width={250}
+									height={300}
+									priority />
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className={styles.center}>
-				<div className={styles.container}>
-					<div className={styles.row}>
-						<div className={styles.column33}>
-							<Image
-								className={styles.imgStyle}
-								src={"/index/weddingSign.png"}
-								alt="WeddingSign"
-								width={250}
-								height={200}
-								priority
-							/>
-						</div>
-						<div className={styles.column66}>
-							<h1 className={styles.sectionTitle}>Wedding Sign</h1>
-							<p>
-								We do Wedding Signs for the most important celebration of your life. Our wedding signs
-								are sure to make your wedding extra special. We offer a wide range of wedding signs,
-								including welcome signs, seating charts, and more! Check out our wedding sign designs
-								and shop for wedding signs!
-							</p>
-							<a href="./wedding-sign" className={styles.button}>
-								Shop
-							</a>
+				<div className={styles.centerHome}>
+					<div className={styles.container}>
+						<div className={styles.row}>
+							<div className={styles.column33}>
+								<Image
+									className={styles.imgStyle}
+									src={"/index/weddingSign.png"}
+									alt="WeddingSign"
+									width={250}
+									height={200}
+									priority />
+							</div>
+							<div className={styles.column66}>
+								<h1 className={styles.sectionTitle}>Wedding Sign</h1>
+								<p>
+									We do Wedding Signs for the most important celebration of your life. Our wedding signs
+									are sure to make your wedding extra special. We offer a wide range of wedding signs,
+									including welcome signs, seating charts, and more! Check out our showcase
+									and shop for wedding signs!
+								</p>
+								<a href="./wedding-sign" className={styles.button}>
+									Wedding Sign Showcase
+								</a>
+								<div class="overlay">
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</main>
+				</div>
+			</main>
 	);
 }
