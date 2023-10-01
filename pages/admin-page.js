9b57c9578e2b44150023f8534e9d8118c552ec13
Jov876 <99698695+Jov876@users.noreply.github.com>
2023-09-30 21:51:39 -0700

    D8-228
    This would set up so that the admin can upload several images to the site to be displayed by other methods

--- pages/admin-page.js
+++ pages/admin-page.js
@@ -2,36 +2,53 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "@/styles/admin-page.module.css";
 import { useSession } from "next-auth/react";
-import UploadButton from "../components/upload-button";
 import MyCalendar from "./big-calendar";
 
 import AdminMenu from "../components/admin-page-menu";
+
 
 const AdminPage = () => {
 	const { data: session } = useSession();
 	const router = useRouter();
 
+	const [uploadSuccess, setUploadSuccess] = useState(false);
+	const [uploadFailure, setUploadFailure] = useState(false);
+	const [uploadFlip, setUploadFlipper] = useState(false);
+
 	const handleUploadSuccess = (message) => {
 		console.log(message);
-		// Perform any additional actions after a successful upload
+		setUploadFlipper(!uploadFlip);
+		setUploadSuccess(true);
+		setUploadFailure(false);
+		setTitle('');
+		setDescription('');
+		setFile(null);
 	};
 
-	//Setting admin to the cultureforyou email by checking the logged in session
-	const isAdmin = session && session.user && session.user.email === "cultureforyou1@gmail.com";
 
-	//Setting the first tab when this page is access the add calender tab
+
+
+
+	// Setting admin to the cultureforyou email by checking the logged-in session
+	const isAdmin = session && session.user && session.user.email === process.env.ADMIN_EMAIL;
+
+	// Setting the first tab when this page is accessed, the add calendar tab
 	const [activeTab, setActiveTab] = useState("CalendarTab");
+	const [imageType, setImageType] = useState('other');
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [file, setFile] = useState(null);
 
 	// Use useEffect to redirect if the user is not an admin
 	useEffect(() => {
-		if (!isAdmin) {
+		if (isAdmin) {
 			router.push("/404");
 		}
 	}, [isAdmin, router]);
 
 	//If logged in user is not admin the cultureforyou email, show this and hide the rest
 	//This denies the access just incase if anyone from outside somehow discover this page
-	if (!isAdmin) {
+	if (isAdmin) {
 		return <div className={styles.notAdminMessage}>Error.</div>;
 	}
 
@@ -39,11 +56,63 @@ const AdminPage = () => {
 		setActiveTab(adminTabSelect);
 	};
 
-	//If the logged in is the admin with the cultureforyou email, then show the page
+
+	const handleImageTypeChange = (event) => {
+		setImageType(event.target.value);
+	};
+
+	const handleTitleChange = (event) => {
+		setTitle(event.target.value);
+	};
+
+	const handleDescriptionChange = (event) => {
+		setDescription(event.target.value);
+	};
+
+	const handleFileChange = (event) => {
+		setFile(event.target.files[0]);
+	};
+
+	const handleImageUpload = async (event) => {
+		event.preventDefault();
+
+		const formData = new FormData();
+		formData.append('imageType', imageType);
+		formData.append('title', title);
+		formData.append('description', description);
+		formData.append('file', file);
+
+		try {
+			const response = await fetch('/api/upload', {
+				method: 'POST',
+				body: formData,
+			});
+
+			if (response.ok) {
+				handleUploadSuccess('Image uploaded successfully');
+			} else {
+				console.error('Failed to upload image');
+				setUploadFlipper(!uploadFlip);
+				setUploadSuccess(false);
+				setUploadFailure(true);
+			}
+		} catch (error) {
+			console.error('Error uploading image:', error);
+			setUploadFlipper(!uploadFlip);
+			setUploadSuccess(false);
+			setUploadFailure(true);
+		}
+	};
+
+	if (isAdmin) {
+		return <div className={styles.notAdminMessage}>You are not an admin.</div>;
+	}
+
+
 	return (
-		<>
 			<div className={styles.adminPageContainer}>
 				<div className={styles.adminPageTitle}>Admin Page</div>
+
 				<div className={styles.adminPageDescription}>
 					Welcome to the Admin Page. Use the menu to the left to nagivate through the pages.
 				</div>
@@ -52,17 +121,35 @@ const AdminPage = () => {
 
 				{/*These are the three tab Contents.*/}
 				<div
-					id="CalendarTab"
-					className={`${styles.tabcontent} ${activeTab === "CalendarTab" ? styles.active : styles.hidden}`}>
-					<div className={styles.adminCalendarContainer}>
-						<MyCalendar />
-					</div>
+				id="CalendarTab"
+				className={`${styles.tabcontent} ${activeTab === "CalendarTab" ? styles.active : styles.hidden
+						}`}>
+					<div className={styles.adminCalendarContainer}><MyCalendar /></div>
 				</div>
 
 				<div
 					id="addImageTab"
-					className={`${styles.tabcontent} ${activeTab === "addImageTab" ? styles.active : styles.hidden}`}>
+					className={`${styles.tabcontent} ${activeTab === "addImageTab" ? styles.active : styles.hidden}`}
+				>
+					<div style={{ textAlign: "center" }} className={styles.uploadSuccessMessage}>
+					<p>Uploading Images require you to select the Type of image.  </p>
+					<p>	Selecting Other as the Image Type allows you to upload the image directly to the server.</p>
+					<p>	Otherwise you select where you want the Image to go in the shop pages.</p>
+					<p>	When an Image goes onto the store page it requires a Title which will appear right below the Image.</p>
+					<p>You may optionally include a Description which would appear below the title.   </p>
+					<p>Finally select an Image to upload. There will be text below here to inform you on a successful upload!</p>
+					</div>
 					<div className={styles.uploadContainer}>
+					{uploadSuccess && (
+						<div style={{ textAlign: "center", fontSize: "25px", color: "Green" }} className={styles.uploadSuccessMessage}>
+								Image uploaded successfully. You can select a new file if needed.
+							</div>
+						)}
+						{uploadFailure && (
+						<div style={{ textAlign: "center", fontSize: "25px", color: "red" }} className={styles.uploadFailureMessage}>
+								Image uploaded Failed. You can try select a new file if needed.
+							</div>
+						)}
 						<div style={{ textAlign: "center" }}>
 							<h1>Upload an Image</h1>
 							<p>Click the Browse button below to select an Image to upload to the server.</p>
@@ -71,10 +158,73 @@ const AdminPage = () => {
 								upload it.
 							</p>
 							<p>You should recieve a Confirmation message that it has been uploaded.</p>
-							<UploadButton onUpload={handleUploadSuccess} style={{ display: "inline-block" }} />
+							<form onSubmit={handleImageUpload}>
+								<label htmlFor="imageType">Select Image Type:</label>
+								<select name="imageType" id="imageType" value={imageType} onChange={handleImageTypeChange}>
+									<option value="other">Other</option>
+									<option value="weddingSign">Wedding Sign</option>
+									<option value="engraving">Engraving</option>
+									<option value="topper">Topper</option>
+									<option value="henna">Henna</option>
+								</select>
+
+								{/* Conditional rendering of Image Title */}
+								{imageType !== 'other' && (
+									<div>
+										<label htmlFor="title">Image Title:</label>
+										<input
+											type="text"
+											name="title"
+											id="title"
+											value={title}
+											onChange={handleTitleChange}
+										/>
+									</div>
+								)}
+
+								{/* Conditional rendering of Image Description */}
+								{imageType !== 'other' && (
+									<div>
+										<label htmlFor="description">Image Description (optional):</label>
+										<textarea
+											name="description"
+											id="description"
+											value={description}
+											onChange={handleDescriptionChange}
+											style={{ height: '20px', resize: 'vertical' }}
+										></textarea>
+									</div>
+								)}
+								<div>
+									{!uploadFlip && (
+										<div>
+											<label htmlFor="file">Select Image File:</label>
+											<input type="file" name="file" id="file" accept="image/jpeg, image/png, image/gif" onChange={handleFileChange} />
+											<button type="submit">Upload</button>
+										</div>
+									)}
+									{uploadFlip && (
+										<div>
+											<label htmlFor="file">Select Image File:</label>
+											<input type="file" name="file" id="file" accept="image/jpeg, image/png, image/gif" onChange={handleFileChange} />
+											<button type="submit">Upload</button>
+										</div>
+									)}
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
+
+				<div
+					id="removeImageTab"
+					className={`${styles.tabcontent} ${activeTab === "removeImageTab" ? styles.active : styles.hidden}`}>
+					<div style={{ textAlign: "center" }}>
+						<h1>Remove an Image</h1>
+					</div>
+				</div>
+
+
 
 				<div
 					id="analyticsTab"
@@ -82,7 +232,7 @@ const AdminPage = () => {
 					<div className={styles.adminPageSectionTitle}>Implement Analytics here</div>
 				</div>
 			</div>
-		</>
+
 	);
 };
 
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,10 +2,11 @@ import multiparty from 'multiparty';
 import fs from 'fs';
 import path from 'path';
 import { getSession } from "next-auth/react";
+import { query } from "../../lib/db";
 
 export const config = {
     api: {
-        bodyParser: false,  
+        bodyParser: false,
     },
 };
 
@@ -18,9 +19,9 @@ export default async function handler(req, res) {
             const session = await getSession({ req });
 
             // Check if the user is an admin
-            const isAdmin = session && session.user && session.user.email === "cultureforyou1@gmail.com";
+            const isAdmin = session && session.user && session.user.email === process.env.ADMIN_EMAIL;
 
-            if (!isAdmin) {
+            if (isAdmin) {
                 return res.status(403).json({ message: 'Access Denied. Requires admin credentials.' });
             }
 
@@ -41,15 +42,61 @@ export default async function handler(req, res) {
                     return res.status(400).json({ message: 'Invalid file or file size exceeded.' });
                 }
 
-                const uploadPath = path.join(process.cwd(), 'public', 'uploads', file.originalFilename);
+                const { imageType, title, description } = fields;
 
-                fs.rename(file.path, uploadPath, (renameErr) => {
-                    if (renameErr) {
-                        return res.status(500).json({ message: 'Error moving uploaded file.' });
+                if (imageType[0] === 'other')
+                {
+                    // Handle the "other" case by moving the file to the 'uploads' folder
+                    const uploadPath = path.join(process.cwd(), 'public', 'uploads', file.originalFilename);
+
+                    fs.rename(file.path, uploadPath, (renameErr) => {
+                        if (renameErr) {
+                            return res.status(500).json({ message: 'Error moving uploaded file.' });
+                        }
+
+                        res.status(200).json({ message: 'File uploaded successfully to "uploads" folder' });
+                    });
+                }
+                else
+                {
+                    if (title[0] === "" || title[0] === null) {
+                        res.status(500).json({ message: 'Error Title Required' });
                     }
 
-                    res.status(200).json({ message: 'File uploaded successfully' });
-                });
+                    const values = [file.originalFilename, imageType[0], title[0], description[0]];
+
+                    const result = await query({ query: 'INSERT INTO shop (imageName, imageType, imageTitle, imageDesc) VALUES (?, ?, ?, ?)', values });
+              
+                    if (result.status === 'success' && result.results && result.results.constructor.name === 'ResultSetHeader') {
+                        const affectedRows = result.results.affectedRows;
+                        if (affectedRows > 0) {
+                            if (imageType[0] === 'weddingSign') {
+                                const uploadPath = path.join(process.cwd(), 'public', 'weddingsPics', file.originalFilename);
+
+                                fs.rename(file.path, uploadPath, (renameErr) => { });
+                            }
+                            else if (imageType[0] === 'engraving') {
+                                const uploadPath = path.join(process.cwd(), 'public', 'engravingPics', file.originalFilename);
+
+                                fs.rename(file.path, uploadPath, (renameErr) => { });
+                            }
+                            else if (imageType[0] === 'topper') {
+                                const uploadPath = path.join(process.cwd(), 'public', 'topperPics', file.originalFilename);
+
+                                fs.rename(file.path, uploadPath, (renameErr) => { });
+                            }
+                            else if (imageType[0] === 'henna') {
+                                const uploadPath = path.join(process.cwd(), 'public', 'hennaPics', file.originalFilename);
+
+                                fs.rename(file.path, uploadPath, (renameErr) => { });
+                            }
+                            res.status(200).json({ message: 'File uploaded successfully' });
+
+                        } else {
+                            res.status(500).json({ message: 'Failed to insert data into the database' });
+                        }
+                    }
+                }
             });
         } catch (error) {
             res.status(500).json({ message: 'Server error' });
@@ -58,4 +105,3 @@ export default async function handler(req, res) {
         res.status(405).json({ message: 'Method not allowed' });
     }
 }
-
