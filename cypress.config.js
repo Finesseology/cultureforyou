d35114fb696047eb844d1ae04df7bd67b69f5ab7
Chris <cwlemke@live.com>
2023-10-31 13:10:01 -0700

    D8-266 D8-258

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
+const { GoogleSocialLogin } = require("cypress-social-logins").plugins;
+//const { GoogleSocialLogin } = require("cypress-social-logins/src/Plugins");
 
 module.exports = defineConfig({
 	e2e: {
 		baseUrl: "http://localhost:3000/",
+		chromeWebSecurity: false,
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
+			on("task", {
+				GoogleSocialLogin: GoogleSocialLogin,
+			});
 		},
 	},
 });
--- pages/sign-in-page.js
+++ pages/sign-in-page.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import styles from "@/styles/account_Styles/sign-in-styles.module.css";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
- function SigningIn() {
-
+function SigningIn() {
 	const { executeRecaptcha } = useGoogleReCaptcha();
 	const { data: session } = useSession();
 	const router = useRouter();
@@ -15,50 +14,41 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 	useEffect(() => {
 		// Check if the user is already signed in, and if so, redirect to another page.
 		if (session) {
-		  router.replace('/');
+			router.replace("/");
 		}
-	  }, [session, router]);
+	}, [session, router]);
 
 	const handleClick = async (e) => {
 		e.preventDefault();
-	  
-		if (!executeRecaptcha) {
-		  console.log("Execute recaptcha not yet available");
-		  return;
-		}
-	  
-		try {
-		  setIsLoading(true); // Set loading state to true
-	  
-		  const gReCaptchaToken = await executeRecaptcha();
-		  console.log(gReCaptchaToken, "response Google reCaptcha server");
-		  await signIn("google", { csrfToken: gReCaptchaToken, reCaptchaResponse: gReCaptchaToken });
-	  
-		  window.alert("Successful Sign in!!");
-		  router.replace('/');
-	  
-		} catch (error) {
-		  console.log(error);
-		} finally {
-		  setIsLoading(false); // Reset loading state to false
-		}
-	  };
-	  
 
-	
+		if (!executeRecaptcha) {
+			console.log("Execute recaptcha not yet available");
+			return;
+		}
+
+		try {
+			setIsLoading(true); // Set loading state to true
+
+			const gReCaptchaToken = await executeRecaptcha();
+			console.log(gReCaptchaToken, "response Google reCaptcha server");
+			await signIn("google", { csrfToken: gReCaptchaToken, reCaptchaResponse: gReCaptchaToken });
+
+			window.alert("Successful Sign in!!");
+			router.replace("/");
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false); // Reset loading state to false
+		}
+	};
 
 	return (
-		
-		<main className={styles.mainContainer} >
+		<main className={styles.mainContainer}>
+			<div className={styles.signInLabelContainer}>
+				<div className={styles.signInLabelContainer}>
+					<div className={styles.signIn_Title}>Sign In</div>
 
-	<div className={styles.signInLabelContainer}>
-		<div className={styles.signInLabelContainer}>
-
-
-			<div className={styles.signIn_Title}>Sign In</div>
-
-		
-				{/*<input className={styles.userInputStyles} type="email" id="email" name="email" placeholder="Enter Email"/>
+					{/*<input className={styles.userInputStyles} type="email" id="email" name="email" placeholder="Enter Email"/>
 
 				<input className={styles.userInputStyles} type="password" id="pwd" name="pwd" placeholder="Enter Password"/>
 
@@ -66,47 +56,38 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 					<input className={styles.checkBoxStyles}  type="checkbox" value="lsRememberMe" id="rememberMe"/> <label for="rememberMe">Remember me</label>
 				</div>*/}
 
+					<div className={styles.signInBtn_Center}>
+						<div className={styles.signInBtn_Container}>
+							<button onClick={handleClick} disabled={isLoading}>
+								{isLoading ? "Signing In..." : "Sign in with Google"}
+							</button>
+						</div>
+					</div>
 
-
-
-		<div className={styles.signInBtn_Center}>
-			<div className={styles.signInBtn_Container}>
-			<button onClick={handleClick} disabled={isLoading}>
-				{isLoading ? 'Signing In...' : 'Sign in with Google'}
-			</button>
-
-			</div>
-			</div>
-			
-			{/*
+					{/*
 				<div className={styles.ForgotCreateContainer}>
 					<a href="./forgot-password-page">Forgot Password</a>
 					<a href="./create-account-page">Create Account</a>
 				</div>
 			*/}
-		</div>
-		
-		</div>
+				</div>
+			</div>
 		</main>
-		
 	);
 }
-
 
 export default function SignInPage(pageProps) {
 	return (
 		<GoogleReCaptchaProvider
-		  reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY}
-		  reCaptchaSecret={process.env.RECAPTCHA_SECRETKEY}
-		  scriptProps={{
-			async: true,
-			defer: false,
-			appendTo: 'head',
-			nonce: undefined,
-		  }}
-		>
-		  <SigningIn {...pageProps} />
+			reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY}
+			reCaptchaSecret={process.env.RECAPTCHA_SECRETKEY}
+			scriptProps={{
+				async: true,
+				defer: false,
+				appendTo: "head",
+				nonce: undefined,
+			}}>
+			<SigningIn {...pageProps} />
 		</GoogleReCaptchaProvider>
-	  );
+	);
 }
-
