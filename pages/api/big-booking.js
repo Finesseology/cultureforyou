715d0095798c0cd66db9f3b52097669ff78c78ab
abdullahHamdan7 <92475314+abHam87@users.noreply.github.com>
2023-11-06 10:39:58 -0800

    Calendar branch (#74)
    * "D8-248"
    
    * "fixed the conflicts - 248"
    
    * D8-259
    
    ---------
    
    Co-authored-by: Chris Lemke <cwlemke@live.com>

--- pages/api/big-booking.js
+++ pages/api/big-booking.js
@@ -7,25 +7,25 @@ export default async function handler(req, res) {
 
         if (req.method === "GET") {
 
-            const querySql = "Select * from appointments order by id ASC"
+            const querySql = `SELECT id, title, DATE_FORMAT(start_time, '%Y %c %d  %h:%i:%s %p') as start_time, DATE_FORMAT(end_time, '%Y %c %d %h:%i:%s %p') as end_time FROM appointmentRequest WHERE status = 'accepted' OR status = 'completed';`;
             const valueParams = [];
             const data = await query({ query: querySql, values: valueParams });
-
-           res.status(200).json({ appointments: data });
+            res.status(200).json({ appointments: data});
         }
 
         // Handle POST request to add new event
         if (req.method === "POST") {
 
-            //console.log(req.body.start);
-            const id = req.body.id;
             const title = req.body.title;
             const endTime = moment(req.body.end).format('YYYY-MM-DD HH:mm:ss');
             const startTime = moment(req.body.start).format('YYYY-MM-DD HH:mm:ss');
+            const userId = req.body.userId;
+            const userName = req.body.userName;
+            const status = req.body.status;
+            
 
-
-            const insertSql = "INSERT INTO appointments (id, title, start_time, end_time) VALUES (?, ?, ?, ?)";
-            const insertParams = [id, title, startTime, endTime];
+            const insertSql = "INSERT INTO appointmentRequest (userId, userName, title, start_time, end_time, status) VALUES (?, ?, ?, ?, ?, ?)";
+            const insertParams = [userId, userName, title, startTime, endTime, status];
             const result = await query({ query: insertSql, values: insertParams });
 
             console.log("inside params: " + insertParams);
@@ -41,27 +41,15 @@ export default async function handler(req, res) {
         if (req.method === "DELETE") {
 
             const eventID = req.query.eventID;
-            console.log("ID received: " + eventID)
-            const deleteSql = "DELETE FROM appointments WHERE id = ?";
+            console.log("ID received: " + eventID)   
+            const deleteSql = "DELETE FROM appointmentRequest WHERE id = ?";
             const deleteParams = [eventID];
             const delData = await query({ query: deleteSql, values: deleteParams });
             
-            // handle updating the events id after deleting
-            const updateSql = "UPDATE appointments SET id = id - 1 WHERE id > ?";
-            const updateParams = [eventID];
-            const updateData = await query({ query: updateSql, values: updateParams });
-
             if (delData.affectedRows > 0) {
                 res.status(201).json({ message: 'Event Deleted successfully' });
             } else {
                 res.status(404).json({ error: 'Failed to delete event' });
-            }
-            
-            // handle error for updating
-            if (updateData.affectedRows > 0) {
-                res.status(201).json({ message: 'Events updated successfully' });
-            } else {
-                res.status(404).json({ error: 'Failed to update event' });
             }
         }
     } catch (error) {
