836ba1bc3365bcd2ec2f95c29240200b0c94613c
Chris Lemke <cwlemke@live.com>
2023-11-07 11:44:05 -0800

    Google-sign-in (#77)
    * D8-266
    D8-258
    
    * D8-258

--- components/navbar-header.js
+++ components/navbar-header.js
@@ -14,24 +14,20 @@ export default function SignInSignOutBar() {
 	const { data: session } = useSession();
 	//Setting admin to the cultureforyou email by checking the logged in session
 
-
-	
-
-
-	
 	if (status === "loading") {
 		return (
 			<nav>
 				<div className={styles.mainContainer}>
 					<div className={styles.webTitle}>
 						<Link className={styles.webTitle_li} href="/">
-						<Image
-									className={styles.image}
-									src={"/homepagePics/logohand.png"}
-									alt="logohand"
-									width={50}
-									height={50}
-									priority />
+							<Image
+								className={styles.image}
+								src={"/homepagePics/logohand.png"}
+								alt="logohand"
+								width={50}
+								height={50}
+								priority
+							/>
 						</Link>
 					</div>
 
@@ -42,10 +38,14 @@ export default function SignInSignOutBar() {
 						<Link className={styles.SignInSignOut_li} href="../appointments">
 							<li>Appointments</li>
 						</Link>
+						<Link className={styles.SignInSignOut_li} href="/shop-layout">
+							<li>Shop</li>
+						</Link>
 						<div className={styles.SignInSignOut_li}>|</div>
 
-						<a href="../sign-in-page" className={styles.signInOutButtonA}>Sign in</a>
-		
+						<a href="../sign-in-page" className={styles.signInOutButtonA}>
+							Sign in
+						</a>
 					</ul>
 				</div>
 			</nav>
@@ -55,16 +55,17 @@ export default function SignInSignOutBar() {
 	return (
 		<nav className={styles.mainContainer}>
 			<div className={styles.webTitle}>
-						<Link className={styles.webTitle_li} href="/">
-						<Image
-									className={styles.image}
-									src={"/homepagePics/logohand.png"}
-									alt="logohand"
-									width={50}
-									height={50}
-									priority />
-						</Link>
-					</div>
+				<Link className={styles.webTitle_li} href="/">
+					<Image
+						className={styles.image}
+						src={"/homepagePics/logohand.png"}
+						alt="logohand"
+						width={50}
+						height={50}
+						priority
+					/>
+				</Link>
+			</div>
 			<ul className={styles.SignInSignOut_ul}>
 				{/*Only isAdmin logged in will show this admin tab*/}
 				<Link className={styles.SignInSignOut_li} href="/">
@@ -83,7 +84,9 @@ export default function SignInSignOutBar() {
 
 				<div className={styles.SignInSignOut_li}>|</div>
 
-				<button className={styles.signInOutButton} onClick={() => signOut()}>Sign out</button>
+				<button className={styles.signInOutButton} onClick={() => signOut()}>
+					Sign out
+				</button>
 			</ul>
 		</nav>
 	);
