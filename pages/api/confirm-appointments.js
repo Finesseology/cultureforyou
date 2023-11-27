19cde1b357166deb66cb4acf0122172e2eff02b8
annasark <annasrkian@gmail.com>
2023-11-27 14:14:59 -0800

    api-admin fixes

--- pages/api/confirm-appointments.js
+++ pages/api/confirm-appointments.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         if (req.method === "GET") {
             const { status } = req.query;
             //const querySql = "SELECT title, DATE_FORMAT(start_time, '%a %b %d %Y %h:%i:%s %p') as start_time, DATE_FORMAT(end_time, '%a %b %d %Y %h:%i:%s %p') as end_time FROM appointments";
-            const querySql = "SELECT * FROM appointmentrequest WHERE status = ?"
+            const querySql = "SELECT * FROM appointmentRequest WHERE status = ?"
             //const valueParams = [];
             const data = await query({ query: querySql, values: [status] });
            res.status(200).json({ appointments: data });
